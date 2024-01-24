e3ffaa3c1fb19b4363bd7c8c9f53887d087dbc01
RomanL91 <dev_roman91@mail.ru>
2024-01-24 17:27:27 +0600

    регистрация в админке + имя приложения

--- app_stock_observer/admin.py
+++ app_stock_observer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_stock_observer.models import CourseJournal
+
+
+@admin.register(CourseJournal)
+class CourseJournalAdmin(admin.ModelAdmin):
+    list_display = [
+        'section', 'exchange_rate', 'date_time',
+    ]
+    readonly_fields = [
+        'section', 'exchange_rate', 'date_time',
+    ]
--- app_stock_observer/apps.py
+++ app_stock_observer/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppStockObserverConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_stock_observer'
+    verbose_name ='Журнал биржевого наблюдателя'
